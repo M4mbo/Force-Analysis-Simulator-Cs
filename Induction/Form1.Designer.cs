e64b2866e3080ca181cb9713ce8893c08d3a9a54
Matias Barandiaran <115642529+M4mbo@users.noreply.github.com>
2023-03-13 11:28:39 +0100

    Add files via upload

---
+++ Induction/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TestInduction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Column1
+            // 
+            this.Column1.Name = "Column1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(713, 113);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(432, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Velocity";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Block mass:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bar mass: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Magnetic field(magnitude):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resistance: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Length: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(184, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(184, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(184, 169);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(184, 223);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(270, 516);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Get Acceleration";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Get Position";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(713, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 613);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Induction/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TestInduction
+{
+    public partial class Form1 : Form
+    {
+
+
+        public environment ambiente;
+        public BindingSource source;
+        public List<dataRow> Datos;
+        public Form1()
+        {
+            InitializeComponent();
+            source = new BindingSource();
+            Datos = new List<dataRow>();
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                particle particula = new particle(Convert.ToDouble(textBox2.Text), new Vector3D(0, 0, 0));
+                ambiente = new environment(new Vector3D(9.81, 0, 0), new Vector3D(Convert.ToDouble(textBox3.Text), 0, 0), Convert.ToDouble(textBox6.Text), 0);
+                double mFieldX = Convert.ToDouble(textBox3.Text);
+                if (mFieldX == 0)
+                {
+                    label6.Text = "Magnetic field needs to be different from 0";
+                }
+                else
+                {
+                    ambiente.t = 0;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Datos.Add(
+                        new dataRow(particula.getVelocity(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, ambiente.t, particula.getAcceleration(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, particula.getPosition(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x));
+                        ambiente.t = (i + 1) * 0.2;
+                    }
+                    source.DataSource = Datos;
+                    source.ResetBindings(false);
+                    dataGridView1.DataSource = source;
+                    foreach (DataGridViewColumn column in dataGridView1.Columns)
+                    {
+                        column.Width = 60;
+                    }
+                    chart1.Series[0] = new Series("Vx");
+                    chart1.Series[0].ChartType = SeriesChartType.Line;
+                    chart1.ChartAreas[0].AxisX.Title = dataGridView1.Columns[1].DataPropertyName + " (s)";
+                    chart1.ChartAreas[0].AxisY.Title = dataGridView1.Columns[0].DataPropertyName + " (m/s)";
+                    chart1.Series[0].XValueMember = dataGridView1.Columns[1].DataPropertyName;
+                    chart1.Series[0].YValueMembers = dataGridView1.Columns[0].DataPropertyName;
+                    chart1.DataSource = dataGridView1.DataSource;
+                }
+            }
+            catch(Exception error)
+            {
+                label6.Text = error.Message + " Please fill in the blank spaces";
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                particle particula = new particle(Convert.ToDouble(textBox2.Text), new Vector3D(0, 0, 0));
+                ambiente = new environment(new Vector3D(9.81, 0, 0), new Vector3D(Convert.ToDouble(textBox3.Text), 0, 0), Convert.ToDouble(textBox6.Text), 0);
+                double mFieldX = Convert.ToDouble(textBox3.Text);
+
+                ambiente.t = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Datos.Add(
+                    new dataRow(particula.getVelocity(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, ambiente.t, particula.getAcceleration(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, particula.getPosition(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x));
+                    ambiente.t = (i + 1) * 0.2;
+                }
+                source.DataSource = Datos;
+                source.ResetBindings(false);
+                dataGridView1.DataSource = source;
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    column.Width = 60;
+                }
+                chart1.Series[0] = new Series("Ax");
+                chart1.Series[0].ChartType = SeriesChartType.Line;
+                chart1.ChartAreas[0].AxisX.Title = dataGridView1.Columns[1].DataPropertyName + " (s)";
+                chart1.ChartAreas[0].AxisY.Title = dataGridView1.Columns[2].DataPropertyName + " (m/s^2)";
+                chart1.Series[0].XValueMember = dataGridView1.Columns[1].DataPropertyName;
+                chart1.Series[0].YValueMembers = dataGridView1.Columns[2].DataPropertyName;
+                chart1.DataSource = dataGridView1.DataSource;
+            }
+            catch (Exception error)
+            {
+                label6.Text = error.Message + " Please fill in the blank spaces";
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                particle particula = new particle(Convert.ToDouble(textBox2.Text), new Vector3D(0, 0, 0));
+                ambiente = new environment(new Vector3D(9.81, 0, 0), new Vector3D(Convert.ToDouble(textBox3.Text), 0, 0), Convert.ToDouble(textBox6.Text), 0);
+                double mFieldX = Convert.ToDouble(textBox3.Text);
+
+                ambiente.t = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Datos.Add(
+                    new dataRow(particula.getVelocity(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, ambiente.t, particula.getAcceleration(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x, particula.getPosition(ambiente, Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox1.Text)).x));
+                    ambiente.t = (i + 1) * 0.2;
+                }
+                source.DataSource = Datos;
+                source.ResetBindings(false);
+                dataGridView1.DataSource = source;
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    column.Width = 60;
+                }
+                chart1.Series[0] = new Series("Px");
+                chart1.Series[0].ChartType = SeriesChartType.Line;
+                chart1.ChartAreas[0].AxisX.Title = dataGridView1.Columns[1].DataPropertyName + " (s)";
+                chart1.ChartAreas[0].AxisY.Title = dataGridView1.Columns[3].DataPropertyName + " (m)";
+                chart1.Series[0].XValueMember = dataGridView1.Columns[1].DataPropertyName;
+                chart1.Series[0].YValueMembers = dataGridView1.Columns[3].DataPropertyName;
+                chart1.DataSource = dataGridView1.DataSource;
+            }
+            catch (Exception error)
+            {
+                label6.Text = error.Message + " Please fill in the blank spaces";
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            dataGridView1.Columns.Clear();
+            source = new BindingSource();
+            Datos = new List<dataRow>();
+        }
+    }
+    public class particle
+    {
+        public double mass { get; set; }
+        public Vector3D position { get; set; }
+        public particle()
+        {
+            mass = 1; position = new Vector3D();
+        }
+        public particle(double Mass, Vector3D Position)
+        {
+            mass = Mass; position = Position;
+        }
+        public Vector3D getVelocity(environment env, double length, double massB) => new Vector3D((env.gField.x * massB * env.resistance * (1 - Math.Pow(Math.E, ((- env.mField.x * env.mField.x * length * length * env.t) / (env.resistance * (mass + massB)))))) / (env.mField.x * env.mField.x * length * length), (env.gField.y * massB * env.resistance * (1 - Math.Pow(Math.E, ((-env.mField.y * env.mField.y * length * length * env.t) / (env.resistance * (mass + massB)))))) / (env.mField.y * env.mField.y * length * length), (env.gField.z * massB * env.resistance * (1 - Math.Pow(Math.E, ((-env.mField.z * env.mField.z * length * length * env.t) / (env.resistance * (mass + massB)))))) / (env.mField.z * env.mField.z * length * length));
+        public Vector3D getAcceleration(environment env, double length, double massB) => new Vector3D((env.gField.x * massB * (Math.Pow(Math.E, ((-env.mField.x * env.mField.x * length * length * env.t) / (env.resistance * (mass + massB)))))) / (mass + massB), (env.gField.y * massB * (Math.Pow(Math.E, ((-env.mField.y * env.mField.y * length * length * env.t) / (env.resistance * (mass + massB)))))) / (mass + massB), (env.gField.z * massB * (Math.Pow(Math.E, ((-env.mField.x* env.mField.z * length * length * env.t) / (env.resistance * (mass + massB)))))) / (mass + massB));
+        public Vector3D getPosition(environment env, double length, double massB) => new Vector3D((env.gField.x * massB * env.resistance * ((env.resistance * (mass + massB)) * (Math.Pow(Math.E, ((-env.mField.x * env.mField.x * length * length * env.t) / (env.resistance * (mass + massB)))) - 1) + env.mField.x * env.mField.x * length * length * env.t)) / (Math.Pow(env.mField.x, 4) * Math.Pow(length, 4)), (env.gField.y * massB * env.resistance * ((env.resistance * (mass + massB)) * (Math.Pow(Math.E, ((-env.mField.y * env.mField.y * length * length * env.t) / (env.resistance * (mass + massB)))) - 1) + env.mField.y * env.mField.y * length * length * env.t)) / (Math.Pow(env.mField.y, 4) * Math.Pow(length, 4)), (env.gField.z * massB * env.resistance * ((env.resistance * (mass + massB)) * (Math.Pow(Math.E, ((-env.mField.z * env.mField.z * length * length * env.t) / (env.resistance * (mass + massB)))) - 1) + env.mField.z * env.mField.z * length * length * env.t)) / (Math.Pow(env.mField.z, 4) * Math.Pow(length, 4)));
+}
+    public class environment
+    {
+        //propiedades
+        public Vector3D gField { get; set; }
+        public Vector3D mField { get; set; }
+        public double resistance { get; set; }
+        public double t { get; set; }
+            //funciones
+            public environment()
+        {
+            gField = new Vector3D(); mField = new Vector3D(); resistance = 1; t = 1;
+        }
+        public environment(Vector3D GField, Vector3D MField, double Resistance, double T)
+        {
+            gField = GField; mField = MField; resistance = Resistance; t = T; 
+        }
+    }
+    public class Vector3D
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public Vector3D() { x = 0; y = 0; z = 0; }
+        public Vector3D(double X, double Y, double Z)
+        {
+            x = X; y = Y; z = Z;
+        }
+    }
+    public class dataRow
+    {
+        public double Vx { get; set; }
+        public double t { get; set; }
+        public double Ax { get; set; }
+        public double Px { get; set; }
+        public dataRow() { Vx = 0; t = 0; Ax = 0; Px = 0; }
+        public dataRow(double VX, double T, double AX, double PX)
+        { Vx = VX; t = T; Ax = AX; Px = PX; }
+    }
+
+
+}
+
+
+
+
